1d79f30827e68cb3972969e16ff9c212d5f94a90
Filip Ramstedt <magra@host-193-11-186-202.eduroam.bth.se>
2016-09-20 14:23:17 +0200

    added functionality

--- public/js/factories/node.factory.js
+++ public/js/factories/node.factory.js
@@ -1,28 +1,3 @@
-var app = angular.module('app', ['ngRoute']);
-app.config(function($locationProvider, $routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'public/views/home.html',
-		controller: 'homeCtrl'
-	}).otherwise({ redirectTo: '/' });
-});
-
-app.controller('homeCtrl', ["$scope", "node", function($scope, node){
-
-    //$scope.title = "AIDS OSKAR!!!!"
-
-    $scope.network = new vis.Network(document.getElementById('c'), { nodes: new vis.DataSet(node.nodes), edges: new vis.DataSet(node.edges) }, {});
-    
-    $scope.network.on( 'select', function(params) {
-        if(params.nodes == ""){
-            console.log(params.edges);
-        }
-        else{
-            console.log(params.nodes);
-        }
-    });
-
-}]);
-
 (function() {
  
    angular.module('app').factory('node', node);
