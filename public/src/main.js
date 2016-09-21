de9160c3394bf16d647278a45b0ee408cece78ae
Nils Löfgren <dkk2@host-193-11-185-9.eduroam.bth.se>
2016-09-21 13:48:12 +0200

    Scrolltop Animation

--- public/src/main.js
+++ public/src/main.js
@@ -11,6 +11,7 @@ app.controller('homeCtrl', ["$scope", "node", function($scope, node){
 
     $scope.splash = true;
     
+    $scope.mediaBoard = false;
     
     $scope.desc = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, magni. Nobis ullam adipisci voluptates consequatur eveniet deleniti expedita optio, vero quam culpa molestias facere laudantium enim vitae accusamus atque possimus odit voluptatem distinctio repellat eaque, error commodi et nulla.";
     
@@ -24,10 +25,19 @@ app.controller('homeCtrl', ["$scope", "node", function($scope, node){
     $scope.title = null;
     $scope.groups = "grp1";
     
+    $scope.changeMediaState = function(){
+        $scope.mediaBoard = !$scope.mediaBoard;
+    }
+    
+    $scope.scrollTop = function(){
+        $("#media_sidebar").animate({ scrollTop: 0 }, "fast");
+    }
+    
     $scope.network = new vis.Network(document.getElementById('c'), { nodes: new vis.DataSet(node.nodes), edges: new vis.DataSet(node.edges) }, {});
     
     $scope.network.on( 'select', function(params) {
         if(params.nodes == ""){
+            $scope.scrollTop();
             console.log("a",params.edges);
             if(params.edges.length == 0){
               $scope.$apply(function() {
@@ -36,6 +46,7 @@ app.controller('homeCtrl', ["$scope", "node", function($scope, node){
             } 
         }
         else{
+            $scope.scrollTop();
             console.log("b", params.nodes[0]);
             $scope.$apply(function() {
                 $scope.title = params.nodes[0];
